386586c39a9bcdd8d33407bc1f0fd931fedfe153
Lee-KyuHwun <wxyz0904@naver.com>
2023-10-16 16:52:53 +0900

    feature: #1-init
    프로젝트를 위한 기본 세팅
    자바17
    스프링 부트 3.1.4 사용

---
+++ src/main/java/com/fastcampus/project/sns/SnsApplication.java
@@ -0,0 +1,14 @@
+package com.fastcampus.project.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)// DB 설정을 하지 않아도 실행되도록 설정
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
